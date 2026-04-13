c09cf739969945bb59e6bc57eb905c71410dbc3d
Kenny <auguste.kenny@monboreal.ca>
2026-04-12 22:36:57 -0400

    Update et correction

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from models.voiture import Voiture
 from services.services_voiture import ServicesVoiture
 
 service = ServicesVoiture()
-
-liste_voitures = service.appeller_voiture()
-for v in liste_voitures:
-    print(v)
-
+mv1 = Voiture("Ferrari", "Spider", 1999, 345000.00,212)
+service.changer_voiture(mv1)
 
 
 
